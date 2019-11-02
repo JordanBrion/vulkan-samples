3da10b987d2baa673da36630032dcf6f179ba33d
Jordan BRION <jrd.brion@gmail.com>
2019-11-02 01:04:51 +0100

    Use /bin dir in /src for multiple executables

---
+++ src/bin/000_validation_layers.rs
@@ -0,0 +1,186 @@
+extern crate ash;
+
+use ash::version::EntryV1_0;
+use ash::version::InstanceV1_0;
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+#[allow(dead_code)]
+unsafe fn print_instance_layers(entry: &ash::Entry) {
+    //&dyn ash::version::EntryV1_1<Instance = ash::vk::Instance>) {
+    let instance_layers_properties = entry
+        .enumerate_instance_layer_properties()
+        .expect("Cannot find instance layer properties");
+
+    for i in 0..instance_layers_properties.len() {
+        let layer_name: [c_char; 256] = instance_layers_properties[i].layer_name;
+        let c_str = CStr::from_ptr(layer_name.as_ptr())
+            .to_str()
+            .expect("Cannot convert instance layer");
+        println!("instance layer name = {}", c_str);
+    }
+}
+
+#[allow(dead_code)]
+unsafe fn print_instance_extensions(entry: &ash::Entry) {
+    let instance_extensions = entry
+        .enumerate_instance_extension_properties()
+        .expect("Cannot retrieve instance extension properties");
+    println!("instance extensions: {}", instance_extensions.len());
+    for i in 0..instance_extensions.len() {
+        let extension_properties: ash::vk::ExtensionProperties = instance_extensions[i];
+        let extension_name = extension_properties.extension_name;
+        let c_str = CStr::from_ptr(extension_name.as_ptr())
+            .to_str()
+            .expect("Cannot convert instance extensions");
+        println!("instance extensions name = {}", c_str);
+    }
+}
+
+fn create_application() -> ash::vk::ApplicationInfo {
+    let application = ash::vk::ApplicationInfo {
+        s_type: ash::vk::StructureType::APPLICATION_INFO,
+        p_next: std::ptr::null(),
+        p_application_name: std::ffi::CString::new("My 1st Vulkan Rust application")
+            .expect("CString::new failed")
+            .as_ptr(),
+        application_version: ash::vk_make_version!(0, 0, 1),
+        p_engine_name: CString::new("Unreal Engine 4")
+            .expect("CString::new failed")
+            .as_ptr(),
+        engine_version: ash::vk_make_version!(0, 0, 1),
+        api_version: ash::vk_make_version!(1, 0, 0),
+    };
+    application
+}
+
+unsafe extern "system" fn debug_callback(
+    _message_severity: ash::vk::DebugUtilsMessageSeverityFlagsEXT,
+    _message_types: ash::vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const ash::vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut std::ffi::c_void,
+) -> ash::vk::Bool32 {
+    println!(
+        "validation layer: {}",
+        CStr::from_ptr((*p_callback_data).p_message)
+            .to_str()
+            .expect("Cannot convert debug message to Rust string")
+    );
+    1
+}
+
+unsafe fn create_instance(
+    entry: &ash::Entry,
+    application_info: ash::vk::ApplicationInfo,
+) -> ash::Instance {
+    let _instance_extensions: std::vec::Vec<*const i8> = vec![
+        ash::extensions::ext::DebugReport::name().as_ptr(),
+        ash::extensions::ext::DebugUtils::name().as_ptr(),
+    ];
+    let _debug_messenger_create_info = ash::vk::DebugUtilsMessengerCreateInfoEXT {
+        s_type: ash::vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+        p_next: std::ptr::null(),
+        flags: Default::default(),
+        message_severity: ash::vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+            | ash::vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+            | ash::vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        message_type: ash::vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+            | ash::vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+            | ash::vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+        pfn_user_callback: Some(debug_callback),
+        p_user_data: std::ptr::null_mut(),
+    };
+    let layers =
+        vec![CString::new("VK_LAYER_LUNARG_standard_validation").expect("Cannot create c-string")];
+    let instance_create_info = ash::vk::InstanceCreateInfo {
+        s_type: ash::vk::StructureType::INSTANCE_CREATE_INFO,
+        p_next: Box::into_raw(Box::new(_debug_messenger_create_info)) as *const std::ffi::c_void,
+        flags: Default::default(),
+        p_application_info: &application_info,
+        enabled_layer_count: layers.len() as u32,
+        pp_enabled_layer_names: layers.as_ptr() as *const *const c_char,
+        enabled_extension_count: _instance_extensions.len() as u32,
+        pp_enabled_extension_names: _instance_extensions.as_ptr(),
+    };
+    let instance: ash::Instance = entry
+        .create_instance(&instance_create_info, None)
+        .expect("Instance creation error");
+    instance
+}
+
+unsafe fn pick_up_one_gpu(
+    instance: &ash::Instance,
+) -> Result<ash::vk::PhysicalDevice, &'static str> {
+    let physical_devices = instance
+        .enumerate_physical_devices()
+        .expect("Cannot enumerate physical devices");
+    println!("number of physical device {}", physical_devices.len());
+    if physical_devices.is_empty() {
+        Err("Cannot get physical devices because none was found")
+    } else {
+        Ok(physical_devices[0])
+    }
+}
+
+fn pick_up_one_queue_family(
+    queue_families_properties: Vec<ash::vk::QueueFamilyProperties>,
+    flags: ash::vk::QueueFlags,
+) -> Result<usize, &'static str> {
+    for i in 0..queue_families_properties.len() {
+        if queue_families_properties[i].queue_flags & flags == flags {
+            return Ok(i);
+        }
+    }
+    Err("No suitable queue family found")
+}
+
+unsafe fn create_logical_device(
+    instance: &ash::Instance,
+    gpu: ash::vk::PhysicalDevice,
+    index_of_queue_family: usize,
+) -> ash::Device {
+    let queue_priority = 1.0_f32;
+    let device_queue_create_info = ash::vk::DeviceQueueCreateInfo {
+        s_type: ash::vk::StructureType::COMMAND_BUFFER_BEGIN_INFO,
+        // s_type: ash::vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: Default::default(),
+        queue_family_index: index_of_queue_family as u32,
+        queue_count: 1,
+        p_queue_priorities: &queue_priority as *const f32,
+    };
+    let device_features = instance.get_physical_device_features(gpu);
+    let device_create_info = ash::vk::DeviceCreateInfo {
+        s_type: ash::vk::StructureType::COMMAND_BUFFER_BEGIN_INFO,
+        // s_type: ash::vk::StructureType::DEVICE_CREATE_INFO,
+        p_next: std::ptr::null(),
+        flags: Default::default(),
+        queue_create_info_count: 1 as u32,
+        p_queue_create_infos: &device_queue_create_info as *const ash::vk::DeviceQueueCreateInfo,
+        enabled_layer_count: 0 as u32,
+        pp_enabled_layer_names: std::ptr::null(),
+        enabled_extension_count: 0 as u32,
+        pp_enabled_extension_names: std::ptr::null(),
+        p_enabled_features: &device_features as *const ash::vk::PhysicalDeviceFeatures,
+    };
+    let logical_device: ash::Device = instance
+        .create_device(gpu, &device_create_info, None)
+        .expect("Cannot create device");
+    logical_device
+}
+
+fn main() {
+    unsafe {
+        let entry = ash::Entry::new().expect("Cannot create entry");
+        // print_instance_extensions(&entry);
+        // print_instance_layers(&entry);
+        let instance = create_instance(&entry, create_application());
+        let gpu = pick_up_one_gpu(&instance).expect("Cannot get physical device");
+        let queue_families_properties = instance.get_physical_device_queue_family_properties(gpu);
+        let index_of_queue_family =
+            pick_up_one_queue_family(queue_families_properties, ash::vk::QueueFlags::COMPUTE)
+                .expect("Cannot get queue family property");
+        let _logical_device = create_logical_device(&instance, gpu, index_of_queue_family);
+    }
+}
