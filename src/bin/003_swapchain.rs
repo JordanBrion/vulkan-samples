b7c8226a51a4b23d8e3bcf3d57a6fb1f43c13889
Jordan BRION <jrd.brion@gmail.com>
2019-12-17 05:24:27 +0100

    [WIP] 003_swapchain: graphics pipeline creation

--- src/bin/003_swapchain.rs
+++ src/bin/003_swapchain.rs
@@ -94,6 +94,21 @@ unsafe fn create_logical_device(
     instance.create_device(*gpu, &device_create_info, None)
 }
 
+unsafe fn create_shader_module(
+    logical_device: &ash::Device,
+    shaderPath: &str,
+) -> ash::vk::ShaderModule {
+    let mut shader_files =
+        std::fs::File::open(shaderPath).expect("Something went wrong when opening shader");
+    let shader_instructions =
+        ash::util::read_spv(&mut shader_files).expect("Failed to read shader spv file");
+    let shader_module_create_infos =
+        ash::vk::ShaderModuleCreateInfo::builder().code(shader_instructions.as_slice());
+    logical_device
+        .create_shader_module(&shader_module_create_infos, None)
+        .expect("Cannot create shader module")
+}
+
 fn main() {
     unsafe {
         let sdl_context = sdl2::init().unwrap();
@@ -113,105 +128,92 @@ fn main() {
         let logical_device = create_logical_device(&instance, &gpu, index_of_queue_family)
             .expect("Cannot create logical device");
 
-        let mut shader_files = [
-        std::fs::File::open(
-            // TODO spv vertex shader
-        "/home/jordanbrion/Documents/rust/vk_001_compute_pipeline/shaders/002_compute_pipeline_2_buffers.comp.spv",
-    )
-    .expect("Something went wrong when opening vertex shader"),
-            std::fs::File::open(
-                // TODO spv fragment shader
-        "/home/jordanbrion/Documents/rust/vk_001_compute_pipeline/shaders/002_compute_pipeline_2_buffers.comp.spv",
-    )
-    .expect("Something went wrong when opening fragment shader"),
-    ];
-        let v_shader_instructions = [
-            ash::util::read_spv(&mut shader_files[0])
-                .expect("Failed to read vertex shader spv file"),
-            ash::util::read_spv(&mut shader_files[1])
-                .expect("Failed to read fragment shader spv file"),
-        ];
-        let v_shader_module_create_infos = [
-            ash::vk::ShaderModuleCreateInfo::builder().code(v_shader_instructions[0].as_slice()),
-            ash::vk::ShaderModuleCreateInfo::builder().code(v_shader_instructions[1].as_slice()),
-        ];
-
         let shader_entry_name =
             CString::new("main").expect("Cannot create vertex shader entry name");
         let v_pipeline_shader_stage_create_infos = [
-            ash::vk::PipelineShaderStageCreateInfo {
-                s_type: ash::vk::StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
-                p_next: std::ptr::null(),
-                flags: Default::default(),
-                stage: ash::vk::ShaderStageFlags::VERTEX,
-                module: logical_device
-                    .create_shader_module(&v_shader_module_create_infos[0], None)
-                    .expect("Cannot create shader module for vertex shader"),
-                p_name: shader_entry_name.as_ptr(),
-                p_specialization_info: std::ptr::null(),
-            },
-            ash::vk::PipelineShaderStageCreateInfo {
-                s_type: ash::vk::StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
-                p_next: std::ptr::null(),
-                flags: Default::default(),
-                stage: ash::vk::ShaderStageFlags::FRAGMENT,
-                module: logical_device
-                    .create_shader_module(&v_shader_module_create_infos[0], None)
-                    .expect("Cannot create shader module for fragment shader"),
-                p_name: shader_entry_name.as_ptr(),
-                p_specialization_info: std::ptr::null(),
-            },
-        ];
-        
-            let window_width = 1280;
-            let window_height = 720;
+                    ash::vk::PipelineShaderStageCreateInfo {
+                        s_type: ash::vk::StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
+                        p_next: std::ptr::null(),
+                        flags: Default::default(),
+                        stage: ash::vk::ShaderStageFlags::VERTEX,
+                        // TODO path
+                        module: create_shader_module(&logical_device, "/home/jordanbrion/Documents/rust/vk_001_compute_pipeline/shaders/002_compute_pipeline_2_buffers.comp.spv"),
+                        p_name: shader_entry_name.as_ptr(),
+                        p_specialization_info: std::ptr::null(),
+                    },
+                    ash::vk::PipelineShaderStageCreateInfo {
+                        s_type: ash::vk::StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
+                        p_next: std::ptr::null(),
+                        flags: Default::default(),
+                        stage: ash::vk::ShaderStageFlags::FRAGMENT,
+                        // TODO path
+                        module: create_shader_module(&logical_device, "/home/jordanbrion/Documents/rust/vk_001_compute_pipeline/shaders/002_compute_pipeline_2_buffers.comp.spv"),
+                        p_name: shader_entry_name.as_ptr(),
+                        p_specialization_info: std::ptr::null(),
+                    },
+                ];
+
+        let window_width = 1280;
+        let window_height = 720;
 
         let viewport = ash::vk::Viewport {
             x: 0f32,
-y: 0f32,
-width: window_width as f32,
-height: window_height as f32,
-min_depth: 0.0,
-max_depth: 1.0
-        }
+            y: 0f32,
+            width: window_width as f32,
+            height: window_height as f32,
+            min_depth: 0.0,
+            max_depth: 1.0,
+        };
         let viewport_state_create_info = ash::vk::PipelineViewportStateCreateInfo {
             s_type: ash::vk::StructureType::PIPELINE_VIEWPORT_STATE_CREATE_INFO,
-p_next: std::ptr::null(),
-flags: Default::default(),
-viewport_count: 1,
-p_viewports: &viewport,
-scissor_count: 0,
-p_scissors: std::ptr::null()
+            p_next: std::ptr::null(),
+            flags: Default::default(),
+            viewport_count: 1,
+            p_viewports: &viewport,
+            scissor_count: 0,
+            p_scissors: std::ptr::null(),
         };
 
         let rasterization_state_create_info = ash::vk::PipelineRasterizationStateCreateInfo {
             s_type: ash::vk::StructureType::PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
             p_next: std::ptr::null(),
             flags: Default::default(),
-            depth_clamp_enable: true as Bool32,
-            rasterizer_discard_enable: true as Bool32,
+            depth_clamp_enable: ash::vk::TRUE,
+            rasterizer_discard_enable: ash::vk::TRUE,
             polygon_mode: ash::vk::PolygonMode::FILL,
             cull_mode: ash::vk::CullModeFlags::BACK,
             front_face: ash::vk::FrontFace::CLOCKWISE,
-            depth_bias_enable: false as Bool32,
+            depth_bias_enable: ash::vk::FALSE,
             depth_bias_constant_factor: 0f32,
             depth_bias_clamp: 0f32,
             depth_bias_slope_factor: 0f32,
-            line_width: 1f32
+            line_width: 1f32,
         };
 
-                let graphics_pipeline_create_info = ash::vk::GraphicsPipelineCreateInfo {
-        s_type: ash::vk::StructureType::GRAPHICS_PIPELINE_CREATE_INFO,
-        p_next: std::ptr::null(),
-        flags: ash::vk::PipelineCreateFlags::DISABLE_OPTIMIZATION,
-        stage_count: v_pipeline_shader_stage_create_infos.len(),
-        p_stages: v_pipeline_shader_stage_create_infos.as_ptr(),
-        p_vertex_input_state: std::ptr::null(),
-        p_input_assembly_state: std::ptr::null(),
-        p_tessellation_state: std::ptr::null(),
-        p_viewport_state: &viewport_state_create_info,
-        p_rasterization_state: &rasterization_state_create_info,
-        // p_multisample_state: *const PipelineMultisampleStateCreateInfo
+        let multisample_state_create_info = ash::vk::PipelineMultisampleStateCreateInfo {
+            s_type: ash::vk::StructureType::PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+            p_next: std::ptr::null(),
+            flags: Default::default(),
+            rasterization_samples: ash::vk::SampleCountFlags::TYPE_1,
+            sample_shading_enable: ash::vk::FALSE,
+            min_sample_shading: 0f32,
+            p_sample_mask: std::ptr::null(),
+            alpha_to_coverage_enable: ash::vk::FALSE,
+            alpha_to_one_enable: ash::vk::FALSE,
+        };
+
+        //                 let graphics_pipeline_create_info = ash::vk::GraphicsPipelineCreateInfo {
+        //         s_type: ash::vk::StructureType::GRAPHICS_PIPELINE_CREATE_INFO,
+        //         p_next: std::ptr::null(),
+        //         flags: ash::vk::PipelineCreateFlags::DISABLE_OPTIMIZATION,
+        //         stage_count: v_pipeline_shader_stage_create_infos.len(),
+        //         p_stages: v_pipeline_shader_stage_create_infos.as_ptr(),
+        //         p_vertex_input_state: std::ptr::null(),
+        //         p_input_assembly_state: std::ptr::null(),
+        //         p_tessellation_state: std::ptr::null(),
+        //         p_viewport_state: &viewport_state_create_info,
+        //         p_rasterization_state: &rasterization_state_create_info,
+        //         p_multisample_state: &multisample_state_create_info,
         // p_depth_stencil_state: *const PipelineDepthStencilStateCreateInfo
         // p_color_blend_state: *const PipelineColorBlendStateCreateInfo
         // p_dynamic_state: *const PipelineDynamicStateCreateInfo
@@ -221,34 +223,35 @@ p_scissors: std::ptr::null()
         // base_pipeline_handle: Pipeline
         // base_pipeline_index: i32
         //         };
-                logical_device.create_graphics_pipelines(ash::vk::PipelineCache::null(),
-                                                        &[graphics_pipeline_create_info],
-                                                        None);
+        // logical_device.create_graphics_pipelines(ash::vk::PipelineCache::null(),
+        //                                         &[graphics_pipeline_create_info],
+        //                                         None);
 
-        let mut canvas = window.into_canvas().build().unwrap();
-        canvas.set_draw_color(Color::RGB(0, 255, 255));
-        canvas.clear();
-        canvas.present();
-        let mut event_pump = sdl_context.event_pump().unwrap();
-        let mut i = 0;
-        'running: loop {
-            i = (i + 1) % 255;
-            canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-            canvas.clear();
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit { .. }
-                    | Event::KeyDown {
-                        keycode: Some(Keycode::Escape),
-                        ..
-                    } => break 'running,
-                    _ => {}
-                }
-            }
-            // The rest of the game loop goes here...
+        // uncomment those lines
+        // let mut canvas = window.into_canvas().build().unwrap();
+        // canvas.set_draw_color(Color::RGB(0, 255, 255));
+        // canvas.clear();
+        // canvas.present();
+        // let mut event_pump = sdl_context.event_pump().unwrap();
+        // let mut i = 0;
+        // 'running: loop {
+        //     i = (i + 1) % 255;
+        //     canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        //     canvas.clear();
+        //     for event in event_pump.poll_iter() {
+        //         match event {
+        //             Event::Quit { .. }
+        //             | Event::KeyDown {
+        //                 keycode: Some(Keycode::Escape),
+        //                 ..
+        //             } => break 'running,
+        //             _ => {}
+        //         }
+        //     }
+        //     // The rest of the game loop goes here...
 
-            canvas.present();
-            ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        }
+        //     canvas.present();
+        //     ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        // }
     }
 }
